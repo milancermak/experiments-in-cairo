5542609ab39165a34110ca1e45dd1c85fd4b2d6c
Milan Cermak <milan.cermak@gmail.com>
2022-06-15 14:32:14 +0200

    chore: bump to v0.9.0

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ import pytest
 
 from starkware.starknet.business_logic.state.state import BlockInfo
 from starkware.starknet.compiler.compile import compile_starknet_files
-from starkware.starknet.services.api.contract_definition import ContractDefinition
+from starkware.starknet.services.api.contract_class import ContractClass
 from starkware.starknet.testing.starknet import Starknet
 
 
@@ -14,7 +14,7 @@ def contract_dir() -> str:
     return os.path.join(here, "..", "contracts")
 
 
-def compile_contract(contract_name: str) -> ContractDefinition:
+def compile_contract(contract_name: str) -> ContractClass:
     contract_src = os.path.join(contract_dir(), contract_name)
     return compile_starknet_files(
         [contract_src], debug_info=True, disable_hint_validation=True, cairo_path=[contract_dir()]
@@ -35,103 +35,103 @@ async def starknet():
 @pytest.fixture(scope="module")
 async def account_contract(starknet):
     contract = compile_contract("dummy_account.cairo")
-    return await starknet.deploy(contract_def=contract, constructor_calldata=[])
+    return await starknet.deploy(contract_class=contract, constructor_calldata=[])
 
 
 @pytest.fixture(scope="module")
 async def state(starknet):
     contract = compile_contract("state.cairo")
-    return await starknet.deploy(contract_def=contract)
+    return await starknet.deploy(contract_class=contract)
 
 
 @pytest.fixture(scope="module")
 async def calls(starknet):
     contract = compile_contract("calls.cairo")
-    return await starknet.deploy(contract_def=contract)
+    return await starknet.deploy(contract_class=contract)
 
 
 @pytest.fixture(scope="module")
 async def targets(starknet):
     contract = compile_contract("targets.cairo")
-    return await starknet.deploy(contract_def=contract)
+    return await starknet.deploy(contract_class=contract)
 
 
 @pytest.fixture(scope="module")
 async def echo1(starknet):
     contract = compile_contract("echo.cairo")
-    return await starknet.deploy(contract_def=contract)
+    return await starknet.deploy(contract_class=contract)
 
 
 @pytest.fixture(scope="module")
 async def echo2(starknet):
     contract = compile_contract("echo.cairo")
-    return await starknet.deploy(contract_def=contract)
+    return await starknet.deploy(contract_class=contract)
 
 
 @pytest.fixture(scope="module")
 async def only_funcs(starknet):
     contract = compile_contract("deeper/only_funcs.cairo")
-    return await starknet.deploy(contract_def=contract)
+    return await starknet.deploy(contract_class=contract)
 
 
 @pytest.fixture(scope="module")
 async def debug_hints(starknet):
     contract = compile_contract("debug_hints.cairo")
-    return await starknet.deploy(contract_def=contract)
+    return await starknet.deploy(contract_class=contract)
 
 
 @pytest.fixture(scope="module")
 async def various(starknet):
     contract = compile_contract("various.cairo")
-    return await starknet.deploy(contract_def=contract)
+    return await starknet.deploy(contract_class=contract)
 
 
 @pytest.fixture(scope="module")
 async def jmp_loop(starknet):
     contract = compile_contract("arr/jmp_loop.cairo")
-    return await starknet.deploy(contract_def=contract)
+    return await starknet.deploy(contract_class=contract)
 
 
 @pytest.fixture(scope="module")
 async def using_invoke(starknet):
     contract = compile_contract("blackmagic/using_invoke.cairo")
-    return await starknet.deploy(contract_def=contract)
+    return await starknet.deploy(contract_class=contract)
 
 
 @pytest.fixture(scope="module")
 async def mocking_internals(starknet):
     contract = compile_contract("test_mocking_internals.cairo")
-    return await starknet.deploy(contract_def=contract)
+    return await starknet.deploy(contract_class=contract)
 
 
 @pytest.fixture(scope="module")
 async def visibility_main(starknet):
     contract = compile_contract("visibility/main.cairo")
-    return await starknet.deploy(contract_def=contract)
+    return await starknet.deploy(contract_class=contract)
 
 
 @pytest.fixture(scope="module")
 async def dicts(starknet):
     contract = compile_contract("dicts.cairo")
-    return await starknet.deploy(contract_def=contract)
+    return await starknet.deploy(contract_class=contract)
 
 
 @pytest.fixture(scope="module")
 async def foo(starknet):
     contract = compile_contract("foo.cairo")
-    return await starknet.deploy(contract_def=contract)
+    return await starknet.deploy(contract_class=contract)
 
 
 @pytest.fixture(scope="module")
 async def tkn(starknet):
     contract = compile_contract("token/tkn.cairo")
-    return await starknet.deploy(contract_def=contract)
+    return await starknet.deploy(contract_class=contract)
 
 
 @pytest.fixture(scope="module")
 async def arrstructs(starknet):
     contract = compile_contract("blackmagic/arrstructs.cairo")
-    return await starknet.deploy(contract_def=contract)
+    return await starknet.deploy(contract_class=contract)
 
 
 @pytest.fixture
